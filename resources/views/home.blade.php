a1c7f3f105733c31ccaf4061977fc2a72c6e476e
JamesJJT <jastevens2222@hotmail.co.uk>
2019-07-07 19:47:36 +0100

    Add card for Fantasy

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,26 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header">Dashboard</div>
                 <div class="card-body">
                     <p>Welcome: {{$user->name}}</p>
                     <p>{{$user->email}}</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header align-content-center align-content-end">
+                    Fantasy Team
+                </div>
+                <div class="card-body">
+                    {{--Replace this with the object that will detect if the user has a team already --}}
+                    @if (!isset($fantasyvreatebutton))
+                        <p>You have no current team, create one with the button below.</p>
+                        <a class="btn btn-primary float ml-1 mt-1" href="#">Create a Team</a>
+                    @endif
                 </div>
             </div>
         </div>
