23df0e63130414b991fb91be6ae3f2ff82312220
JamesJJT <jastevens2222@hotmail.co.uk>
2019-07-07 16:41:36 +0100

    Reverse the order of the seaons, latest at the bottom

--- resources/views/seasons/pastRaces.blade.php
+++ resources/views/seasons/pastRaces.blade.php
@@ -2,8 +2,15 @@
 
 @section('content')
     <div class="container">
-        @foreach($f1json->MRData->SeasonTable->Seasons as $year)
-            <a href="{{route('specificSeason', [$year->season])}}" class="list-group-item">{{$year->season}}</a>
+        <h1 class="text-center">Seasons</h1>
+        @foreach(array_chunk(array_reverse($f1json->MRData->SeasonTable->Seasons),6) as $year)
+            <div class="row mb-2">
+                @foreach($year as $f1season)
+                    <div class="col-md-2">
+                        <a href="{{route('specificSeason', [$f1season->season])}}" class="list-group-item text-center">{{$f1season->season}}</a>
+                    </div>
+                @endforeach
+            </div>
         @endforeach
     </div>
 
