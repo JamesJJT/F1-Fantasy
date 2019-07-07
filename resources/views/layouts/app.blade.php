d8cb576f4edce9df1f42f7b05231a7517d3b79f7
JamesJJT <jastevens2222@hotmail.co.uk>
2019-07-07 16:18:46 +0100

    Changes to nav bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,8 +33,15 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('races')}}">Races <span class="sr-only">(current)</span></a>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                Seasons <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item border-bottom" href="{{route('races')}}">Current Season</a>
+                                <a class="dropdown-item border-bottom" href="{{route('pastRaces')}}">List of Seasons</a>
+                            </div>
                         </li>
                         <li class="nav-item">
                             <a href="{{route('drivers')}}" class="nav-link">Drivers</a>
